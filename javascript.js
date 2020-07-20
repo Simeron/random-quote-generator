51a398c5d4564e012a6702882478f0604ba7d256
Simeron Taak <simeron03@icloud.com>
2020-07-20 07:21:50 +0100

    refactored the code a bit - for clearer code

--- javascript.js
+++ javascript.js
@@ -1,41 +1,26 @@
-// const { stringify } = require("querystring");
-
 const quoteBox = document.querySelector('#text');
 const currentAuthor = document.querySelector('#author');
 const newQuote = document.querySelector('#new-quote');
 const tweettheQuote = document.querySelector('#tweet-quote');
 
-// let requestURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-// let request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
-// request.onload = function () {
-//   const jsonRawData = request.response;
-//   // var data = JSON.parse(jsonRawData);
-//   var quotes = jsonRawData.quotes;
-//   var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-//   console.log(randomQuote);
-// };
-
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      var jsonRawData = this.responseText;
-      var data = JSON.parse(jsonRawData);
-      var quotes = data.quotes;
+      const jsonRawData = this.responseText;
+      const data = JSON.parse(jsonRawData);
+      const quotes = data.quotes;
       //gets the data above
 
-      var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
       console.log(randomQuote);
       //gets a random quote
 
-      var getAuthor = randomQuote.author; //change to getAuthor
+      let getAuthor = randomQuote.author; 
       console.log(getAuthor)
       //gets a random author
 
-      var getQuote = randomQuote.quote; // change to getQuote
+      let getQuote = randomQuote.quote; 
       console.log(getQuote);
       //gets a random quote
 
@@ -44,8 +29,8 @@ function loadDoc() {
       console.log(quoteBox);
       console.log(currentAuthor);
 
-      var tweetQuote = randomQuote.quote + " - " + randomQuote.author;
-      tweetthisquote(tweetQuote);
+      let tweetQuote = randomQuote.quote + " - " + randomQuote.author;
+      tweetCurrentQuote(tweetQuote);
     }
 
   };
@@ -60,19 +45,15 @@ function loadNewQuote() {
 
 newQuote.addEventListener('click', loadNewQuote);
 
-function tweetthisquote(tweetQuote) {
-  var url = "https://twitter.com/intent/tweet?text=" + tweetQuote;
+
+
+
+function tweetCurrentQuote(tweetQuote) {
+  let url = "https://twitter.com/intent/tweet?text=" + tweetQuote;
   window.open(url);
   console.log('what am i');
   console.log(tweetQuote);
 };
 
-tweettheQuote.addEventListener('click', tweetthisquote);
+tweettheQuote.addEventListener('click', tweetCurrentQuote);
 
-// function tweetthisquote(newRandomQuote) {
-//   var getQuote = newRandomQuote.quote;
-//   var getAuthor = newRandomQuote.author;
-//   console.log(getQuote, getAuthor);
-// }
-
-// tweetQuote.addEventListener('click', tweetthisquote);
